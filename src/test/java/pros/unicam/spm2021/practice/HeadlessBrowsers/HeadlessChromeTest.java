c0a6f674be0c9a967b307443b7e4255506022ed7
Fabrizio <fabrizio.fornari@unicam.it>
2021-12-02 10:22:59 +0100

    enabling a headless test

--- src/test/java/pros/unicam/spm2021/practice/HeadlessBrowsers/HeadlessChromeTest.java
+++ src/test/java/pros/unicam/spm2021/practice/HeadlessBrowsers/HeadlessChromeTest.java
@@ -60,7 +60,7 @@ public class HeadlessChromeTest
 		address="http://localhost:8080/spm2021";
 		}
 		if(System.getProperty("os.name").contains("Windows")) {
-			System.setProperty("webdriver.chrome.driver", projectPath+"\\drivers\\windows\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\studente\\Documents\\BrowserDriver\\chromedriver.exe");
 			address="http://localhost/spm2021";
 		}
 	    
